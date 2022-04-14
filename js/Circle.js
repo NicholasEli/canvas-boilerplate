ab90650f3d092cda03e078b59ceb4ff0e1bcccd0
Nicholas Eli <nicholaseli@Nicholass-MacBook-Air.local>
2022-04-14 06:57:12 -0700

    Add context correctly

--- js/Circle.js
+++ js/Circle.js
@@ -1,5 +1,6 @@
 export default class Circle {
-	constructor(x, y, radius, color) {
+	constructor(ctx, x, y, radius, color) {
+		this.ctx = ctx;
 		this.x = x;
 		this.y = y;
 		this.radius = radius;
@@ -7,11 +8,11 @@ export default class Circle {
 	}
 
 	draw() {
-		c.beginPath();
-		c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-		c.fillStyle = this.color;
-		c.fill();
-		c.closePath();
+		this.ctx.beginPath();
+		this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+		this.ctx.fillStyle = this.color;
+		this.ctx.fill();
+		this.ctx.closePath();
 	}
 
 	update() {
--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 import Canvas from './Canvas.js';
+import Cirlce from './Circle.js';
 
 window.onload = function () {
 	console.log('--Loading Javascript');
 	const canvas = new Canvas();
 	canvas.init();
-	console.log(canvas);
+
+	const circle1 = new Cirlce(10, 10, 10, '#ff0000');
+	circle1.draw();
 };
