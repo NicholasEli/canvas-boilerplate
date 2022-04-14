079380868505d9daa987c5b4c168e3d792414c3a
Nicholas Eli <nicholaseli@Nicholass-MacBook-Air.local>
2022-04-14 07:14:21 -0700

    Return second object

--- js/script.js
+++ js/script.js
@@ -10,12 +10,12 @@ window.onload = function () {
 	const circle1 = new Cirlce(canvas, 30, 30, 30, 'red');
 	circle1.draw();
 
-	// const circle2 = new Cirlce(
-	// 	canvas.ctx,
-	// 	canvas.dimensions.width - 30,
-	// 	canvas.dimensions.height - 30,
-	// 	30,
-	// 	'blue'
-	// );
-	// circle2.draw();
+	const circle2 = new Cirlce(
+		canvas,
+		canvas.dimensions.width - 30,
+		canvas.dimensions.height - 30,
+		30,
+		'blue'
+	);
+	circle2.draw();
 };
