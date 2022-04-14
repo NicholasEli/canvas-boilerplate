eec990c14a00d0df54e0cb4c8778a8584837dfd7
Nicholas Eli <nicholaseli@Nicholass-MacBook-Air.local>
2022-04-14 07:02:02 -0700

    doh

--- js/utils.js
+++ js/utils.js
@@ -1,17 +1,17 @@
 /**
  * Calcs distance between two points
-**/ 
-export const distance function (x1, y1, x2, y2) {
-  const xDist = x2 - x1
-  const yDist = y2 - y1
+ **/
+export function distance(x1, y1, x2, y2) {
+  const xDist = x2 - x1;
+  const yDist = y2 - y1;
 
-  return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2))
+  return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
 }
 
 /**
  * Animate elements on a loop
-**/
-export const animate function(ctx, canvas ) {
+ **/
+export function animate(ctx, canvas) {
   requestAnimationFrame(animate);
   ctx.clearRect(0, 0, canvas.dimensions.width, canvas.dimensions.height);
 }
