3b3b42b44609bd00c337eb4c6097b5401be3201e
Nicholas Eli <nicholaseli@Nicholass-MacBook-Air.local>
2022-04-14 07:00:57 -0700

    Add animation helper

--- js/utils.js
+++ js/utils.js
@@ -1,9 +1,17 @@
 /**
  * Calcs distance between two points
-* */ 
+**/ 
 export const distance function (x1, y1, x2, y2) {
   const xDist = x2 - x1
   const yDist = y2 - y1
 
   return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2))
+}
+
+/**
+ * Animate elements on a loop
+**/
+export const animate function(ctx, canvas ) {
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvas.dimensions.width, canvas.dimensions.height);
 }
