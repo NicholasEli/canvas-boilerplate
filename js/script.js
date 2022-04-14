94a33bc8375dced6fb69f67b4b02298d59346ce6
Nicholas Eli <nicholaseli@Nicholass-MacBook-Air.local>
2022-04-14 09:16:41 -0700

    comment out collision example

--- js/script.js
+++ js/script.js
@@ -21,37 +21,37 @@ window.onload = function () {
 
 	/**
 	 * Animate circles for collision
-	 **/
-	let reverse = false;
-	animate(
-		canvas,
-		() => {
-			const d = distance(circle1.x, circle1.y, circle2.x, circle2.y);
+		let reverse = false;
+		animate(
+			canvas,
+			() => {
+				const d = distance(circle1.x, circle1.y, circle2.x, circle2.y);
 
-			if (collision(d, circle1.radius, circle2.radius)) {
-				reverse = true;
-			}
+				if (collision(d, circle1.radius, circle2.radius)) {
+					reverse = true;
+				}
 
-			if (circle1.x <= 0 || (circle1.x > 0 && !reverse)) {
-				reverse = false;
-				circle1.x = circle1.x + 1;
-				circle1.y = circle1.y + 1;
+				if (circle1.x <= 0 || (circle1.x > 0 && !reverse)) {
+					reverse = false;
+					circle1.x = circle1.x + 1;
+					circle1.y = circle1.y + 1;
 
-				circle2.x = circle2.x - 1;
-				circle2.y = circle2.y - 1;
-			}
+					circle2.x = circle2.x - 1;
+					circle2.y = circle2.y - 1;
+				}
 
-			if (reverse) {
-				circle1.x = circle1.x - 1;
-				circle1.y = circle1.y - 1;
+				if (reverse) {
+					circle1.x = circle1.x - 1;
+					circle1.y = circle1.y - 1;
 
-				circle2.x = circle2.x + 1;
-				circle2.y = circle2.y + 1;
-			}
+					circle2.x = circle2.x + 1;
+					circle2.y = circle2.y + 1;
+				}
 
-			circle1.update();
-			circle2.update();
-		},
-		false
-	);
+				circle1.update();
+				circle2.update();
+			},
+			false
+		);
+	**/
 };
