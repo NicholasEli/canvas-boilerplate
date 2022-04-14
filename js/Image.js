aa976192364924035c586b3b8d908f3a6c3d94f0
Nicholas Eli <nicholaseli@Nicholass-MacBook-Air.local>
2022-04-14 09:45:51 -0700

    Create versitile image class for rendering inner image

---
+++ js/Image.js
@@ -0,0 +1,62 @@
+/**
+ * Image Class
+ * @param { class } canvas - Canvas Class
+ * @param { int } sx - x coordinate of image relative to container x axis
+ * @param { int } sy - y cooridnate of relative to container y axis
+ * @param { int } sWidth - width of image relative to container
+ * @param { int } height - height of image relative to container
+ * @param { int } x - x coordinate
+ * @param { int } y - y cooridnate
+ * @param { int } width - width of rectangle
+ * @param { int } height - height of rectangle
+ * @param { string } color - color of rectangle
+ **/
+export default class Rectangle {
+	constructor(
+		canvas,
+		sx = null,
+		sy = null,
+		sWidth = null,
+		sHeight = null,
+		x,
+		y,
+		width,
+		height,
+		image
+	) {
+		this.canvas = canvas;
+		this.sx = sx;
+		this.sy = sy;
+		this.sWidth = sWidth;
+		this.sHeight = sHeight;
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.image = image;
+	}
+
+	draw() {
+		this.canvas.ctx.beginPath();
+		if (this.sx === null || this.sy === null || !this.sWidth === null || !this.sHeight === null) {
+			this.canvas.ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+		} else {
+			this.canvas.ctx.drawImage(
+				this.image,
+				this.sx,
+				this.sy,
+				this.sWidth,
+				this.sHeight,
+				this.x,
+				this.y,
+				this.width,
+				this.height
+			);
+		}
+		this.canvas.ctx.closePath();
+	}
+
+	update() {
+		this.draw();
+	}
+}
--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 import Canvas from './Canvas.js';
 import Cirlce from './Circle.js';
 import Rectangle from './Rectangle.js';
+import Image from './Image.js';
 import { animate, distance, collision } from './utils.js';
 
 window.onload = function () {
@@ -22,4 +23,37 @@ window.onload = function () {
 
 	const rectangle = new Rectangle(canvas, 0, canvas.dimensions.height - 30, 60, 30, 'green');
 	rectangle.draw();
+
+	const character = {
+		width: 860,
+		height: 625,
+		el: document.getElementById('character'),
+	};
+	const image1 = new Image(
+		canvas,
+		null,
+		null,
+		null,
+		null,
+		canvas.dimensions.width / 2,
+		canvas.dimensions.height / 2,
+		50,
+		50,
+		character.el
+	);
+	image1.draw();
+
+	const image2 = new Image(
+		canvas,
+		0,
+		0,
+		character.width / 2,
+		character.height / 2,
+		canvas.dimensions.width / 2,
+		canvas.dimensions.height / 2 + 50,
+		50,
+		50,
+		character.el
+	);
+	image2.draw();
 };
